dc30e493f05c753fc819f39111b9de7d91453b93
ryandev7 <110151138+ryandev7@users.noreply.github.com>
2023-01-31 20:24:55 +0900

    [level 1] Title: 행렬의 덧셈, Time: 28.47 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        // 2차원 배열 공부 다시하기
+        
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for(int i = 0; i < arr1.length; i++){
+            
+            for(int j = 0; j < arr1[0].length; j++){
+                
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+                
+            }
+            
+        }
+        return answer;
+    }
+}
