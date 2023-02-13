92335d3084a7a10e75c7d663bad7df0ee384a57b
ryandev7 <110151138+ryandev7@users.noreply.github.com>
2023-02-13 19:23:33 +0900

    [level 1] Title: 최소직사각형, Time: 1.90 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        
+        int long_max = 1;
+        int short_max = 1;
+        
+        for(int i = 0; i < sizes.length; i++){
+            int longer = Math.max(sizes[i][0], sizes[i][1]);
+            int shorter = Math.min(sizes[i][0], sizes[i][1]);
+            
+            if(longer > long_max){
+                long_max = longer;
+            }
+            if(shorter > short_max){
+                short_max = shorter;
+            }
+        }
+        answer = long_max * short_max;
+        
+        return answer;
+    }
+}
